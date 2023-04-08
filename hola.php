774c182f33795db28c848bd51579b895730f9a82
cruz <cruzdavid015@gmail.com>
2023-04-08 10:58:59 -0430

    probando

---
+++ hola.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+
+<h1>bienvenido a php</h1>
+
+<?php  echo HOLA PHP; ?>
+    
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+/*
+const nombre = prompt("cual es tu nombre");
+document.querySelector('.contenido').innerHTML=`${nombre} esta aprendiendo`;
+
+*/
+
+const producto ="codigo 24 pulgadas";
+document.querySelector('.contenido').innerHTML = `${producto} exitoso`;
+
+console.log(producto);
+
+console.table(producto);
